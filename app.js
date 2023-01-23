30ce8c6fa95dc0a624140b56057d8f6438d3e20d
Nasri Nasir <nasrinasir@MacBook-Pro.myrepublic.com.sg>
2023-01-23 16:04:56 +0800

    replace md5 hash with bcrypt

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-console.log(md5("123456"));
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
@@ -38,35 +37,42 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password:md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
+
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      res.render("secrets");
+      if (foundUser) {
+        bcrypt.compare(password,foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      }
     }
   });
-});
-
-app.post("/login", function(req,res){
-  const username = req.body.username;
-  const password = md5(req.body.password);
-
-User.findOne({email: username}, function(err,foundUser){
-  if (err) {
-    console.log(err);
-  } else {
-    if (foundUser) {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      }
-      }
-    }
-  }
-);
 });
 
 
